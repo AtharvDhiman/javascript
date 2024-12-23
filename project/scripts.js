557c1e0d5b16c184849f07f3a4aaf124609f31d5
AtharvDhiman <ayushdhiman.9997@gmail.com>
2024-12-23 15:38:10 +0530

    first commit

---
+++ project/scripts.js
@@ -0,0 +1,38 @@
+// function add(a,b){
+//     console.log(a+b);
+
+// }
+// add(10,20);
+// var add2=(a,b)=>{
+//     console.log(a+b);
+// };
+// add2(102,20);
+// var add3=(a,b)=>console.log(a+b);
+// add3(30,20);
+
+
+// funtion myfunction()
+// {
+//     var x=5-5;
+//     var y="5"+5;
+//     var z="hello"+5;
+//     var demop=document.getElementById("demo");
+//     demop.innerHTML=x +"<br>"+y+"<br>"+z;
+// }
+
+var myCollection=[
+    1,
+    "meera maam",
+    true,
+    {
+        name:"sanjeev",
+        age:30
+    },
+
+    function(name){
+        console.log(name);
+    },
+["abhishek","bacchan","amitabh","bachchan"]
+
+]
+console.log(myCollection);
